06b1f7f98754e883073a295dd0508924c1dcf6ce
Michael Gorven <michael@gorven.net>
2025-12-09 11:50:25 -0800

    Support non-default schemas

--- alembic_views/autogenerate.py
+++ alembic_views/autogenerate.py
@@ -13,23 +13,46 @@ from alembic_views.operations import CreateViewOp, DropViewOp, ReplaceViewOp
 log = logging.getLogger(__name__)
 
 
-def reflect_sqlite(autogen_context: AutogenContext) -> dict[str, str]:
-    views = autogen_context.connection.execute(  # type: ignore[union-attr]
-        text("SELECT name, sql FROM sqlite_master WHERE type='view'")
-    )
-    return {view[0]: normalise_sqlite(view[1]) for view in views}
+def reflect_sqlite(
+    autogen_context: AutogenContext, schemas: list[str]
+) -> dict[tuple[str | None, str], str]:
+    views: dict[tuple[str | None, str], str] = {}
+    for schema in schemas:
+        prefix = schema + "." if schema else ""
+        rows = autogen_context.connection.execute(  # type: ignore[union-attr]
+            text(f"SELECT name, sql FROM {prefix}sqlite_master WHERE type='view'")
+        )
+        views.update({(schema, row[0]): normalise_sqlite(row[1]) for row in rows})
+    return views
 
 
 def normalise_sqlite(definition: str) -> str:
     return re.sub(r"^CREATE VIEW \S+ AS ", "", definition, flags=re.I).replace("\n", "")
 
 
-def reflect_postgresql(autogen_context: AutogenContext) -> dict[str, str]:
-    views = autogen_context.connection.execute(  # type: ignore[union-attr]
-        text("SELECT viewname, definition FROM pg_views WHERE schemaname = :schema"),
-        {"schema": autogen_context.dialect.default_schema_name},  # type: ignore[union-attr]
+def reflect_postgresql(
+    autogen_context: AutogenContext, schemas: list[str]
+) -> dict[tuple[str | None, str], str]:
+    rows = autogen_context.connection.execute(  # type: ignore[union-attr]
+        text(
+            "SELECT schemaname, viewname, definition FROM pg_views WHERE schemaname = ANY(:schemas)"
+        ),
+        {
+            "schemas": [
+                autogen_context.dialect.default_schema_name  # type: ignore[union-attr]
+                if schema is None
+                else schema
+                for schema in schemas
+            ]
+        },
     )
-    return {view[0]: normalise_postgresql(view[1]) for view in views}
+    return {
+        (
+            None if row[0] == autogen_context.dialect.default_schema_name else row[0],  # type: ignore[union-attr]
+            row[1],
+        ): normalise_postgresql(row[2])
+        for row in rows
+    }
 
 
 def normalise_postgresql(definition: str) -> str:
@@ -48,7 +71,7 @@ REFLECT_DIALECT = {
 def compare_views(
     autogen_context: AutogenContext,
     upgrade_ops: UpgradeOps,
-    schemas: list[str],  # noqa: ARG001
+    schemas: list[str],
 ) -> None:
     sqla_views = {
         k: str(v.compile()).replace("\n", "")
@@ -59,19 +82,26 @@ def compare_views(
         raise NotImplementedError(
             f"Unsupported dialect for view reflection: {autogen_context.dialect.name}"  # type: ignore[union-attr]
         )
-    db_views = REFLECT_DIALECT[autogen_context.dialect.name](autogen_context)  # type: ignore[union-attr]
+    db_views = REFLECT_DIALECT[autogen_context.dialect.name](autogen_context, schemas)  # type: ignore[union-attr]
 
-    for name in set(sqla_views) - set(db_views):
+    for schema, name in set(sqla_views) - set(db_views):
         log.info("Detected added view '%s'", name)
-        upgrade_ops.ops.append(CreateViewOp(name, sqla_views[name]))
+        upgrade_ops.ops.append(CreateViewOp(name, sqla_views[(schema, name)], schema))
 
-    for name in set(db_views) - set(sqla_views):
+    for schema, name in set(db_views) - set(sqla_views):
         log.info("Detected removed view '%s'", name)
-        upgrade_ops.ops.append(DropViewOp(name, old_definition=db_views[name]))
+        upgrade_ops.ops.append(
+            DropViewOp(name, schema, old_definition=db_views[(schema, name)])
+        )
 
-    for name in set(sqla_views) & set(db_views):
-        if sqla_views[name] != db_views[name]:
+    for schema, name in set(sqla_views) & set(db_views):
+        if sqla_views[(schema, name)] != db_views[(schema, name)]:
             log.info("Detected changed view '%s'", name)
             upgrade_ops.ops.append(
-                ReplaceViewOp(name, sqla_views[name], old_definition=db_views[name])
+                ReplaceViewOp(
+                    name,
+                    sqla_views[(schema, name)],
+                    schema,
+                    old_definition=db_views[(schema, name)],
+                )
             )
--- alembic_views/operations.py
+++ alembic_views/operations.py
@@ -9,17 +9,24 @@ from alembic.operations import MigrateOperation, Operations
 
 @Operations.register_operation("create_view")
 class CreateViewOp(MigrateOperation):
-    def __init__(self, name: str, definition: str) -> None:
+    def __init__(self, name: str, definition: str, schema: str | None) -> None:
         self.name = name
         self.definition = definition
+        self.schema = schema
 
     @classmethod
-    def create_view(cls, operations: Operations, name: str, definition: str) -> Any:
-        op = CreateViewOp(name, definition)
+    def create_view(
+        cls,
+        operations: Operations,
+        name: str,
+        definition: str,
+        schema: str | None = None,
+    ) -> Any:
+        op = CreateViewOp(name, definition, schema)
         return operations.invoke(op)
 
     def reverse(self) -> "DropViewOp":
-        return DropViewOp(self.name, self.definition)
+        return DropViewOp(self.name, self.schema)
 
     def to_diff_tuple(self) -> tuple[str, str]:
         return ("create_view", self.name)
@@ -28,14 +35,19 @@ class CreateViewOp(MigrateOperation):
 @Operations.implementation_for(CreateViewOp)
 def create_view(operations: Operations, operation: CreateViewOp) -> None:
     quote = operations.get_bind().engine.dialect.identifier_preparer.quote
-    operations.execute(f"CREATE VIEW {quote(operation.name)} AS {operation.definition}")
+    name = (
+        f"{quote(operation.schema)}.{quote(operation.name)}"
+        if operation.schema
+        else quote(operation.name)
+    )
+    operations.execute(f"CREATE VIEW {name} AS {operation.definition}")
 
 
 @renderers.dispatch_for(CreateViewOp)
 def render_create_view(
     _autogen_context: AutogenContext, operation: CreateViewOp
 ) -> str:
-    return f"op.create_view('{operation.name}', '''{operation.definition}''')"
+    return f"op.create_view({operation.name!r}, '''{operation.definition}''', schema={operation.schema!r})"
 
 
 @Operations.register_operation("replace_view")
@@ -44,19 +56,26 @@ class ReplaceViewOp(MigrateOperation):
         self,
         name: str,
         definition: str,
+        schema: str | None,
         drop: bool = False,
         old_definition: str | None = None,
     ) -> None:
         self.name = name
         self.definition = definition
+        self.schema = schema
         self.drop = drop
         self.old_definition = old_definition
 
     @classmethod
     def replace_view(
-        cls, operations: Operations, name: str, definition: str, drop: bool = False
+        cls,
+        operations: Operations,
+        name: str,
+        definition: str,
+        schema: str | None = None,
+        drop: bool = False,
     ) -> Any:
-        op = ReplaceViewOp(name, definition, drop=drop)
+        op = ReplaceViewOp(name, definition, schema, drop=drop)
         return operations.invoke(op)
 
     def reverse(self) -> "ReplaceViewOp":
@@ -64,6 +83,7 @@ class ReplaceViewOp(MigrateOperation):
         return ReplaceViewOp(
             self.name,
             self.old_definition,
+            self.schema,
             old_definition=self.definition,
         )
 
@@ -74,38 +94,46 @@ class ReplaceViewOp(MigrateOperation):
 @Operations.implementation_for(ReplaceViewOp)
 def replace_view(operations: Operations, operation: ReplaceViewOp) -> None:
     quote = operations.get_bind().engine.dialect.identifier_preparer.quote
+    name = (
+        f"{quote(operation.schema)}.{quote(operation.name)}"
+        if operation.schema
+        else quote(operation.name)
+    )
+
     if operation.drop or operations.get_bind().engine.name == "sqlite":
-        operations.execute(f"DROP VIEW {quote(operation.name)}")
-        operations.execute(
-            f"CREATE VIEW {quote(operation.name)} AS {operation.definition}"
-        )
+        operations.execute(f"DROP VIEW {name}")
+        operations.execute(f"CREATE VIEW {name} AS {operation.definition}")
     else:
-        operations.execute(
-            f"CREATE OR REPLACE VIEW {quote(operation.name)} AS {operation.definition}"
-        )
+        operations.execute(f"CREATE OR REPLACE VIEW {name} AS {operation.definition}")
 
 
 @renderers.dispatch_for(ReplaceViewOp)
 def render_replace_view(
     _autogen_context: AutogenContext, operation: ReplaceViewOp
 ) -> str:
-    return f"op.replace_view('{operation.name}', '''{operation.definition}''')"
+    return f"op.replace_view({operation.name!r}, '''{operation.definition}''', schema={operation.schema!r})"
 
 
 @Operations.register_operation("drop_view")
 class DropViewOp(MigrateOperation):
-    def __init__(self, name: str, old_definition: str | None = None) -> None:
+    def __init__(
+        self, name: str, schema: str | None, old_definition: str | None = None
+    ) -> None:
+        self.schema = schema
         self.name = name
+        self.schema = schema
         self.old_definition = old_definition
 
     @classmethod
-    def drop_view(cls, operations: Operations, name: str) -> Any:
-        op = DropViewOp(name)
+    def drop_view(
+        cls, operations: Operations, name: str, schema: str | None = None
+    ) -> Any:
+        op = DropViewOp(name, schema)
         return operations.invoke(op)
 
     def reverse(self) -> CreateViewOp:
         assert self.old_definition
-        return CreateViewOp(self.name, self.old_definition)
+        return CreateViewOp(self.name, self.old_definition, self.schema)
 
     def to_diff_tuple(self) -> tuple[str, str]:
         return ("drop_view", self.name)
@@ -114,9 +142,14 @@ class DropViewOp(MigrateOperation):
 @Operations.implementation_for(DropViewOp)
 def drop_view(operations: Operations, operation: DropViewOp) -> None:
     quote = operations.get_bind().engine.dialect.identifier_preparer.quote
-    operations.execute(f"DROP VIEW {quote(operation.name)}")
+    name = (
+        f"{quote(operation.schema)}.{quote(operation.name)}"
+        if operation.schema
+        else quote(operation.name)
+    )
+    operations.execute(f"DROP VIEW {name}")
 
 
 @renderers.dispatch_for(DropViewOp)
 def render_drop_view(_autogen_context: AutogenContext, operation: DropViewOp) -> str:
-    return f'op.drop_view("{operation.name}")'
+    return f"op.drop_view({operation.name!r}, schema={operation.schema!r})"
